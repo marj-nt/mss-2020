4c160036e21ef1db28a80fdbb217a7b42929c986
Marjorie Tolentino <marjt1@yahoo.com>
2020-07-15 21:03:37 -0700

    Programs and prices

---
+++ js/cycle-cards.js
@@ -0,0 +1,7 @@
+$('#programs-main button').click( function(e) {
+    $('#programs-main .collapse').collapse('hide');
+});
+
+$('#policies-main button').click( function(e) {
+    $('#policies-main .collapse').collapse('hide');
+});
