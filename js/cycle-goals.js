03b3939e120a27ee5c686e7b3bab12c5362b5fef
Marjorie Tolentino <marjt1@yahoo.com>
2020-07-19 13:55:18 -0700

    Nav and cycle relative paths

--- js/cycle-goals.js
+++ js/cycle-goals.js
@@ -7,7 +7,7 @@ function cycleGoals() {
     var descrs = ['Build confidence and comfort to be an independent swimmer!',
     'Develop proper technique and form!', 'Create a fun and productive learning experience!'], i = 0;
 
-    var pics = ['../images/index/cycle-confidence.png', '../images/index/cycle-technique.png', '../images/index/cycle-fun.png'], i = 0;
+    var pics = ['images/index/cycle-confidence.png', 'images/index/cycle-technique.png', 'images/index/cycle-fun.png'], i = 0;
 
     var word = $('#index-goals figcaption');
     var pic = $('#index-goals img');
