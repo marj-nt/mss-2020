a55845e4d46d8a7ee2326f564febe3f1896ffab7
Marjorie Tolentino <marjt1@yahoo.com>
2020-07-24 12:05:34 -0700

    revert contact test

---
+++ contact-form.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $phone = $_POST['phone'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo = "marjt1@yahoo.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from ".$name.".".\n\n".$message
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: contact.html?mailsend");
+}
